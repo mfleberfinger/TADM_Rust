af3d2bf1fc1ccd36463989d6e040c2e9fbee8077
Matthew <mfleberfinger@gmail.com>
2022-01-30 15:31:05 -0500

    Ruled out dynamic arrays as a good beginner Rust project.

--- src/collections.rs
+++ src/collections.rs
@@ -1,1 +1,39 @@
+/* TODO: Rust doesn't allow dynamic sizing of its standard arrays.
+ * Apparently, implementing a dynamic array requires "advanced" Rust
+ * knowledge, including unsafe code (https://doc.rust-lang.org/nomicon/vec/vec.html).
+ * This doesn't seem like a good place to start.
+#[cfg(test)]
+mod dynamic_array_tests {
 
+    // Make sure a dynamic array accepts data and grows as needed.
+    // Make sure the size and count are updated correctly when items are added.
+    #[test]
+    fn add_items() {
+    }
+
+    // Make sure that attempting to index into unused but allocated space
+    // results in a panic.
+    #[test]
+    #[should_panic(expected = "Attempted to access unitialized dynamic array index.")]
+    fn bounds_check() {
+    }
+}
+
+
+pub struct DynamicArray<T> {
+    size: usize,
+    items: Box<[T]>
+}
+
+impl<T> DynamicArray<T> {
+    pub fn new() -> DynamicArray<T> {
+        DynamicArray {
+            size: 1,
+            items: Box::new([T; 1])
+        }
+    }
+
+    pub fn append(item: T) {
+    }
+}
+*/
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,1 @@
 pub mod collections;
-
-#[cfg(test)]
-mod dynamic_array_tests {
-    #[test]
-    fn array_grows() {
-        // TODO: Make sure a dynamic array accepts data and grows as needed.
-        // Make sure the count is updated correctly when items are added.
-    }
-
-    #[test]
-    fn array_removal_test() {
-        // TODO: Make sure items can be removed from the array.
-        // Make sure the count is updated correctly when items are removed.
-    }
-}
